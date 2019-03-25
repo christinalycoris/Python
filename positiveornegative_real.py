808078f125b5bb2106916fbe919a969d07fbf2ae
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-25 15:59:41 -0700

    Create positiveornegative_real.py

---
+++ positiveornegative_real.py
@@ -0,0 +1,10 @@
+x = float(input("Please enter a real number: "))
+if x < 0:
+	print('Number is negative.')
+elif x > 0:
+	print('Number is positive.')
+elif x == 0:
+	print('Number is neither positve nor negative.\nThe number is zero.')
+else:
+	print('Hmm. I don\'t seem to know that one.')
+
