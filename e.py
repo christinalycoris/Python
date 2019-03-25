87aa7e7af62485777a7a2292012a93baefe211b6
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-25 01:32:36 -0700

    Update e.py

--- e.py
+++ e.py
@@ -1,8 +1,8 @@
 import math
 n = int(input("Enter the number of terms: "))
-sum1 = 1
+sum = 1
 
 for i in range(1,n+1):
-	sum1 = sum1+(1/math.factorial(i))
-print("The sum of series is",round(sum1,10))
+	sum = sum+(1/math.factorial(i))
+print("The sum of series is",round(sum,10))
 
