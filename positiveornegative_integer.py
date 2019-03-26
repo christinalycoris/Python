a2de116a3299e800f2f05cff5f4289227e12121a
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-25 19:57:39 -0700

    Update positiveornegative_integer.py

--- positiveornegative_integer.py
+++ positiveornegative_integer.py
@@ -1,10 +1,10 @@
 x = int(input("Please enter an integer: "))
-if x < 0:
-	print('Number is negative.')
+if x == 0:
+	print('Number is neither positve nor negative.\nThe number is zero.')
 elif x > 0:
 	print('Number is positive.')
-elif x == 0:
-	print('Number is neither positve nor negative.\nThe number is zero.')
+elif x < 0:
+	print('Number is negative.')
 else:
 	print('Hmm. I don\'t seem to know that one.')
 
