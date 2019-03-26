e9daf51a22a5ac2320907442ffe37b75e9682b4a
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-25 19:01:43 -0700

    Update readinput.py

--- readinput.py
+++ readinput.py
@@ -1,5 +1,23 @@
+print("Hello. :)")
+
+name = input("What is your name? ")
+print("Nice to meet you " + name + "!")
+
+age = input("How old are you, " + name + "? ")
+print("So, you are already " + str(age) + " years old, " + name + "!\nCONNGRATULATIONS!!!")
+
+ans = input("Is today your birthday, " + name + "? ")
+if 'Y' in ans or 'y' in ans:
+    print('HAPPY BIRTHDAY!!!')
+else:
+    print('That\'s too bad. :(')
+
 x = int(input("Please enter an integer: "))
 print('Your number is', x)
 #another way to write
 print('Your number is {0}'.format(x))
+
+y = float(input("Please enter an integer: "))
+print("Your number is "+str(y)+".")
+print('Your number is {0}.'.format(y))
 
