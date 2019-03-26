edd5506447b21345b6fd61ae0f2e6ebb03748a3f
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-26 02:39:57 -0700

    Update evenorodd_loop.py

--- evenorodd_loop.py
+++ evenorodd_loop.py
@@ -1,18 +1,18 @@
 print ("This program will determine if the number the user inputs\n"
-    "into the program is even or odd (or neither). Integers only.")
+	"into the program is even or odd (or neither). Integers only.")
 ans = input("Do you want to continue? Y/N ")
 
 while 'Y' in ans or 'y' in ans:
-    num = int(input("Enter a number: "))
-    if num == 0:
-        print("The number zero is neither even nor odd.")
-    elif (num % 2) == 0:
-        print("{0} is Even".format(num))
-    else:
-        print("{0} is Odd".format(num))
-    ans = input("Do you want to continue? Y/N ")
+	num = int(input("Enter a number: "))
+	if num == 0:
+		print("The number zero is neither even nor odd.")
+	elif (num % 2) == 0:
+		print("{0} is even".format(num))
+	else:
+		print("{0} is odd".format(num))
+	ans = input("Do you want to continue? Y/N ")
 
 while 'N' in ans or 'n' in ans: #could also use 'if' in place of 'while'
-    print('That\'s too bad. :(\nGoodbye.')
-    exit()
+	print('That\'s too bad. :(\nGoodbye.')
+	exit()
 
