30b64fd250c65732f91bd07340630e3a6246dd90
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-25 19:20:38 -0700

    Update summation_while.py

--- summation_while.py
+++ summation_while.py
@@ -2,17 +2,17 @@ j = int(input("Please enter a starting number: "))
 k = int(input("Please enter an ending number: "))
 n = int(input("Please enter an increment number: "))
 
-sum=0
-count=0
+sum = 0
+count = 0
 
 while j <= k:
-	sum = sum+j
-	print(j, end=" + ")
-	j = j+n
-	count+=1
-	if j > k:
-		break
+    sum = sum + j
+    print(j, end = " + ")
+    j = j + n
+    count += 1
+    if j > k:
+        break
 print('Loop ended.')
 print('Sum of series is', sum)
-print('Loop ran',count,'times.')
+print('Loop ran', count, 'times.')
 
